fa0cf3478036836c3b462a05748fb9cc1c5dde71
Brian Vu <brianvu7@gmail.com>
2022-02-03 09:47:27 -0600

    commit tree d64b996afdf4394715fa33d5298cdb7774fd4668

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+const superbowlWin = array => {
+    const result = array.find(item => item.result === "W");
+
+    if(!result){
+        return result;
+    }
+    
+    return result.year;
+}
